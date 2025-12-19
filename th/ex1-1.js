2e14570ebb0a349d25c64972123a8c6f36b5eeab
nopparat <nopparat.cysit@gmail.com>
2025-12-19 10:49:30 +0700

    feat: implement callback functions for calculations and notifications in exercises

--- th/ex1-1.js
+++ th/ex1-1.js
@@ -7,7 +7,11 @@
 */
 
 // Start Coding Here
-
+const calculate = (num1,num2,callback) => callback(num1,num2) 
+const add = (num1,num2) => num1 + num2
+const subtract = (num1,num2) => num1 - num2
+const multiply = (num1,num2) => num1 * num2
+const divide = (num1,num2) => (num1 || num2) === 0 ? "Cannot divide by zero" : num1 / num2
 
 // เมื่อ function นี้ทำงานจะเห็นผลลัพธ์ดังนี้
 console.log(calculate(10, 5, add)); // 15
--- th/ex2-2.js
+++ th/ex2-2.js
@@ -1,14 +1,22 @@
 /* 
 1. Callback & Higher-Order Function มีข้อดีอย่างไร
-Answer:
+Answer: ลดความซ้ำซ้อนของโค้ดทำให้ง่ายแก่การอ่านและปรับปรุง แต่ยากสำหรับการทำความเข้าใจ โครงสร้าง ลำดับการทำงาน ต้องใช้เวลาในการทำความเข้าใจ
 
 
 2. จากการลองใช้ Callback Function ในระบบเกมและระบบแจ้งเตือน คุณคิดว่าโค้ดอ่านง่ายขึ้นหรือไม่ เพราะอะไร 
-Answer:
-
+Answer: อ่านง่ายขึ้นเพราะสามารถดูการทำงานของหลายๆฟังชันได้โดยการดูที่ การ call function จะสามารถดูได้ว่า function ไหนทำงานเหมือนกันบ้าง 
+        เช่น
+        console.log(sendNotification(postCreated));  
+        console.log(sendNotification(postLiked));     
+        console.log(sendNotification(postCommented)); 
+        console.log(sendNotification(postShared)); 
+        sendNotification เป็น Higher-Order Function ซื่งเป็น body หลักในการ call function อื่นๆ ที่โค้งสร้าง ไม่ต่างกัน เพื่อลดความซ้ำซ้อน
+        
 
 3. หากต้องสอนเพื่อนให้เข้าใจ Callback & Higher-Order Function ใน 5 นาที จะอธิบายอย่างไร 
-Answer:  
+Answer: Higher-Order Function ฟังก์ชันที่สามารถสั่งให้สร้างฟังก์ชันใหม่ขึ้นมาได้
+        Callback Function ฟังก์ชันที่เราส่งเข้าไปให้หัวหน้า เพื่อให้ Higher เรียกใช้เมื่อถึงเวลาที่เหมาะสม
+        ถ้าเห็นฟังก์ชันไหนที่มีการส่ง "ชื่อฟังก์ชัน" เข้าไปในวงเล็บของอีกฟังก์ชันหนึ่ง ให้เดาได้เลยว่านั่นคือเรื่อง Callback และ Higher
 
 
 */
